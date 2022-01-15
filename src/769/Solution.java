5e1359ec3e6c8f5341ad67b029642ac84957d1b3
Joban <Joban_Cai@163.com>
2022-01-15 11:09:19 +0800

    Initial commit

---
+++ src/769/Solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+
+    public static void main(String[] args) {
+        // 021
+        int[] arr = {0, 5};
+        System.out.println(maxChunksToSorted(arr));
+    }
+
+    public static int maxChunksToSorted(int[] arr) {
+        int res = 0, max = 0;
+        for (int i = 0; i < arr.length; i++){
+            max = Math.max(max, arr[i]);//统计前i个位置的最大元素
+            if (max == i) res++;
+        }
+        return res;
+    }
+
+}
