3c442c117e226728563f57faa9d694b1382507de
蔡俊鹏 <Joban_Cai@163.com>
2022-01-26 15:38:16 +0800

    add

---
+++ src/leetcode/demo_017/Solution.java
@@ -0,0 +1,45 @@
+package leetcode.demo_017;
+
+/**
+ * 对整数的二进制表示取反（0 变 1 ，1 变 0）后，再转换为十进制表示，可以得到这个整数的补数。
+ *
+ * 例如，整数 5 的二进制表示是 "101" ，取反后得到 "010" ，再转回十进制表示得到补数 2 。
+ * 给你一个整数 num ，输出它的补数。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：num = 5
+ * 输出：2
+ * 解释：5 的二进制表示为 101（没有前导零位），其补数为 010。所以你需要输出 2 。
+ * 示例 2：
+ *
+ * 输入：num = 1
+ * 输出：0
+ * 解释：1 的二进制表示为 1（没有前导零位），其补数为 0。所以你需要输出 0 。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/number-complement
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution {
+    
+    public static void main (String[] args) {
+        System.out.println(findComplement(1));
+    }
+
+    public static int findComplement(int num) {
+        char[] array = Integer.toBinaryString(num).toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < array.length; i++) {
+            if ('0' == array[i]) {
+                sb.append('1');
+            } else {
+                sb.append('0');
+            }
+        }
+        return Integer.parseInt(sb.toString(), 2);
+    }
+    
+}
